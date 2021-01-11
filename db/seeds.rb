6c886b522bb66e009ce0d1d2c8732c26d8e3c64c
Thomas Brouillet <thomasbrouillet@gmail.com>
2021-01-11 14:28:22 +0100

    commit before Heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+
 2.times do |i|
     User.create(email: "user-#{i+1}@example.com", password: "password", password_confirmation: "password")
 end
@@ -12,6 +14,6 @@ end
 
 User.all.each do |u|
   10.times do |i|
-    u.todo_items.create(title: "To Do Item #{i+1} for #{u.email}", complete: i % 3 == 0 ? true : false )
+    u.todo_items.create(title: "ToDo Item #{i+1} for #{u.email}", complete: i % 3 == 0 ? true : false )
   end
 end
